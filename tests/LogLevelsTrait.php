dc01163edc67afabf6ff7a3e839edfc63cbc4035
Eric Stern <github@ericstern.com>
2018-03-24 10:35:33 -0700

    Move loglevels into trait since they will be reused across future tests (#2)

---
+++ tests/LogLevelsTrait.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace SimpleLogger;
+
+use Psr\Log\LogLevel;
+
+trait LogLevelsTrait
+{
+    public function allLevels(): array
+    {
+        return [
+            [LogLevel::EMERGENCY],
+            [LogLevel::ALERT],
+            [LogLevel::CRITICAL],
+            [LogLevel::ERROR],
+            [LogLevel::WARNING],
+            [LogLevel::NOTICE],
+            [LogLevel::INFO],
+            [LogLevel::DEBUG],
+        ];
+    }
+}

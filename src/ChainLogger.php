b248f5d8634c219431529d469a35c2f2761af0bd
Eric Stern <eric@ericstern.com>
2018-03-24 11:50:29 -0700

    Rework the chain logger

---
+++ src/ChainLogger.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SimpleLogger;
+
+use Psr\Log\AbstractLogger;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+/**
+ * Handler for multiple loggers
+ *
+ * @package SimpleLogger
+ * @author  Frédéric Guillot
+ */
+class ChainLogger extends Base
+{
+    /**
+     * Logger instances
+     */
+    private $loggers = [];
+
+    public function __construct(array $loggers = [])
+    {
+        foreach ($loggers as $logger) {
+            $this->addLogger($logger);
+        }
+    }
+
+    /**
+     * Sets a logger instance on the object
+     *
+     * @param  LoggerInterface $logger
+     * @return null
+     */
+    public function addLogger(LoggerInterface $logger)
+    {
+        $this->loggers[] = $logger;
+    }
+
+    /**
+     * Proxy method to the real loggers
+     *
+     * @param  mixed   $level
+     * @param  string  $message
+     * @param  array   $context
+     * @return null
+     */
+    protected function writeLog($level, $message, array $context = array())
+    {
+        foreach ($this->loggers as $logger) {
+            $logger->log($level, $message, $context);
+        }
+    }
+}
---
+++ tests/ChainLoggerTest.php
@@ -0,0 +1,104 @@
+<?php
+declare(strict_types=1);
+
+namespace SimpleLogger;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel as LL;
+
+/**
+ * @coversDefaultClass SimpleLogger\ChainLogger
+ * @covers ::<protected>
+ * @covers ::<private>
+ */
+class ChainLoggerTest extends \PHPUnit\Framework\TestCase
+{
+    use BaseTestTrait;
+
+    private $logs = [];
+
+    public function getLogger()
+    {
+        return new ChainLogger();
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testLoggerInConstruct()
+    {
+        $this->assertInstanceOf(
+            ChainLogger::class,
+            new ChainLogger([
+                $this->makeMockLogger(),
+            ])
+        );
+    }
+
+    /**
+     * @covers ::__construct
+     * @expectedException TypeError
+     */
+    public function testNonLoggerInConstruct()
+    {
+        new ChainLogger([new \StdClass()]);
+    }
+
+    /**
+     * @covers ::addLogger
+     */
+    public function testWithOneLogger()
+    {
+        $chain = new ChainLogger();
+        $mock = $this->makeMockLogger();
+        $soh = spl_object_hash($mock);
+        $chain->addLogger($mock);
+
+        $chain->debug('message');
+
+        $this->assertSame([
+            $soh => [
+                [LL::DEBUG, 'message', []],
+            ],
+        ], $this->logs);
+    }
+
+    public function testWithMultipleLoggers()
+    {
+        $chain = new ChainLogger();
+
+        $mock1 = $this->makeMockLogger();
+        $soh1 = spl_object_hash($mock1);
+        $chain->addLogger($mock1);
+
+        $mock2 = $this->makeMockLogger();
+        $soh2 = spl_object_hash($mock2);
+        $chain->addLogger($mock2);
+
+        $chain->debug('message');
+
+        $expected = [
+            $soh1 => [
+                [LL::DEBUG, 'message', []],
+            ],
+            $soh2 => [
+                [LL::DEBUG, 'message', []],
+            ],
+        ];
+        ksort($expected);
+        ksort($this->logs);
+        $this->assertSame($expected, $this->logs);
+    }
+
+    private function makeMockLogger(): LoggerInterface
+    {
+        $mock = $this->createMock(LoggerInterface::class);
+        $soh = spl_object_hash($mock);
+        $this->logs[$soh] = [];
+        $mock->method('log')
+            ->will($this->returnCallback(function (...$args) use ($soh) {
+                $this->logs[$soh][] = $args;
+            }));
+        return $mock;
+    }
+}

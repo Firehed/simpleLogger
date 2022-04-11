82a4d7e7c86b6abe189516bf2d130071cac6a5cb
Eric Stern <eric@ericstern.com>
2022-04-11 15:11:23 -0700

    BC note

--- src/ConfigurableLoggerInterface.php
+++ src/ConfigurableLoggerInterface.php
@@ -6,6 +6,11 @@ namespace Firehed\SimpleLogger;
 
 use Psr\Log\LoggerInterface;
 
+/**
+ * This interface is only intended to be checked by user code. It is not
+ * intended for public implementation, and will not follow strict adherence to
+ * semantic versioning. New methods may be added in minor versions.
+ */
 interface ConfigurableLoggerInterface extends LoggerInterface
 {
     public function setFormat(string $format): void;

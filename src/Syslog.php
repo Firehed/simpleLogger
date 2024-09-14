3a68ea6f7196738f8669851a760cc0ad764f8dda
Eric Stern <eric@ericstern.com>
2024-09-14 12:46:31 -0700

    Fix some namespacing

--- src/Syslog.php
+++ src/Syslog.php
@@ -7,37 +7,31 @@ namespace Firehed\SimpleLogger;
 use Psr\Log\LogLevel;
 use Stringable;
 
-/**
- * Syslog Logger
- *
- * @package SimpleLogger
- * @author  Frédéric Guillot
- */
 class Syslog extends Base
 {
     private const LEVELS = [
-        LogLevel::EMERGENCY => LOG_EMERG,
-        LogLevel::ALERT     => LOG_ALERT,
-        LogLevel::CRITICAL  => LOG_CRIT,
-        LogLevel::ERROR     => LOG_ERR,
-        LogLevel::WARNING   => LOG_WARNING,
-        LogLevel::NOTICE    => LOG_NOTICE,
-        LogLevel::INFO      => LOG_INFO,
-        LogLevel::DEBUG     => LOG_DEBUG,
+        LogLevel::EMERGENCY => \LOG_EMERG,
+        LogLevel::ALERT     => \LOG_ALERT,
+        LogLevel::CRITICAL  => \LOG_CRIT,
+        LogLevel::ERROR     => \LOG_ERR,
+        LogLevel::WARNING   => \LOG_WARNING,
+        LogLevel::NOTICE    => \LOG_NOTICE,
+        LogLevel::INFO      => \LOG_INFO,
+        LogLevel::DEBUG     => \LOG_DEBUG,
     ];
 
     /**
      * Setup Syslog configuration
      *
-     * @param  string $ident    Application name
+     * @param $ident Application name
      * @param  int    $facility See http://php.net/manual/en/function.openlog.php
      */
-    public function __construct($ident = 'PHP', $facility = LOG_USER)
+    public function __construct(string $ident = 'PHP', int $facility = LOG_USER)
     {
         openlog($ident, LOG_ODELAY | LOG_PID, $facility);
     }
 
-    protected function writeLog($level, string|Stringable $message, array $context = array()): void
+    protected function writeLog($level, string|Stringable $message, array $context = []): void
     {
         $syslogPriority = $this->getSyslogPriority($level);
         $syslogMessage = $this->interpolate($message, $context);
@@ -45,11 +39,7 @@ class Syslog extends Base
         syslog($syslogPriority, $syslogMessage);
     }
 
-    /**
-     * @param string $psrLevel PSR log level
-     * @return int LOG_ constant
-     */
-    protected function getSyslogPriority($psrLevel)
+    protected function getSyslogPriority(string $psrLevel): int
     {
         return self::LEVELS[$psrLevel];
     }
